f530bdcc1e52f598bdce1f63a07d547014bb5fbc
baibochun@gmail.com <baibochun@gmail.com@213476b3-89fe-de8f-e45c-0ea09cb4a6d9>
2012-11-07 03:11:41 +0000

    Init

---
+++ trunk/xml2sql/Xml2sql.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace xml2sql
+{
+    partial class Xml2sql
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.documentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtFor = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataOutput = new System.Windows.Forms.DataGridView();
+            this.nameInSQL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameInXML = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataOutput)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(494, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "S&ettings";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "&Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.documentToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // documentToolStripMenuItem
+            // 
+            this.documentToolStripMenuItem.Name = "documentToolStripMenuItem";
+            this.documentToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.documentToolStripMenuItem.Text = "&Documents";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // txtFor
+            // 
+            this.txtFor.Location = new System.Drawing.Point(6, 22);
+            this.txtFor.Multiline = true;
+            this.txtFor.Name = "txtFor";
+            this.txtFor.Size = new System.Drawing.Size(458, 59);
+            this.txtFor.TabIndex = 1;
+            this.txtFor.Text = "/html/body/table[@id=\"tableOne\"]/tr";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtFor);
+            this.groupBox1.Location = new System.Drawing.Point(12, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(470, 88);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "For Every matches";
+            // 
+            // dataOutput
+            // 
+            this.dataOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataOutput.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameInSQL,
+            this.nameInXML});
+            this.dataOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataOutput.Location = new System.Drawing.Point(3, 16);
+            this.dataOutput.Name = "dataOutput";
+            this.dataOutput.RowTemplate.Height = 23;
+            this.dataOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataOutput.Size = new System.Drawing.Size(464, 301);
+            this.dataOutput.TabIndex = 7;
+            // 
+            // nameInSQL
+            // 
+            this.nameInSQL.HeaderText = "Column";
+            this.nameInSQL.Name = "nameInSQL";
+            // 
+            // nameInXML
+            // 
+            this.nameInXML.HeaderText = "Expression";
+            this.nameInXML.Name = "nameInXML";
+            this.nameInXML.Width = 321;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataOutput);
+            this.groupBox3.Location = new System.Drawing.Point(12, 125);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(470, 320);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Output";
+            // 
+            // Xml2sql
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 457);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Xml2sql";
+            this.Text = "xml2sql";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataOutput)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem ;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem ;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem ;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem ;
+        private System.Windows.Forms.ToolStripMenuItem documentToolStripMenuItem ;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtFor;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataOutput;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameInSQL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameInXML;
+    }
+}
+
---
+++ trunk/xml2sql/Xml2sql.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace xml2sql
+{
+    public partial class Xml2sql : Form
+    {
+        public Xml2sql()
+        {
+            InitializeComponent();
+            dataOutput.Rows.Add(new string[] { "Name", "td/div[@class=\"name\"]" });
+            dataOutput.Rows.Add(new string[] { "Age", "td/div[@class=\"age\"" });
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = true;
+            DialogResult result = open.ShowDialog();
+            if (result != DialogResult.OK)
+            {
+                System.Console.WriteLine("Open file canceled.");
+                return;
+            }
+            string[] files = open.FileNames;
+            Dictionary<string, string> columns = new Dictionary<string, string>();
+            foreach (DataGridViewRow row in dataOutput.Rows)
+            {
+                if (row.IsNewRow) continue;
+                columns.Add(row.Cells[0].Value.ToString(), row.Cells[1].Value.ToString());
+            }
+
+            StringBuilder resultText = new StringBuilder();
+
+            foreach (string file in files)
+            {
+                XMLParser parser = new XMLParser(file);
+                List<List<string> > rows = parser.output(txtFor.Text, columns);
+                foreach (List<string> row in rows)
+                {
+                    resultText.Append(file);
+                    foreach (string field in row)
+                    {
+                        resultText.Append("\t").Append(field);
+                    }
+                    resultText.AppendLine();
+                }
+            }
+
+            TextResult formResult = new TextResult();
+            formResult.txtResult.Text = resultText.ToString();
+            formResult.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            DialogResult ret = dialog.ShowDialog();
+            if (ret != DialogResult.OK)
+            {
+                return;
+            }
+            StreamWriter s = new StreamWriter(dialog.OpenFile());
+            s.WriteLine(txtFor.Text);
+            foreach (DataGridViewRow row in dataOutput.Rows)
+            {
+                if (row.IsNewRow) continue;
+                s.WriteLine("{0}\t{1}", row.Cells[0].Value.ToString(), row.Cells[1].Value.ToString());
+            }
+            s.Flush();
+            s.Close();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            DialogResult ret = dialog.ShowDialog();
+            if (ret != DialogResult.OK)
+            {
+                return;
+            }
+            StreamReader r = new StreamReader(dialog.OpenFile());
+            txtFor.Text = r.ReadLine();
+            string line = r.ReadLine();
+            dataOutput.Rows.Clear();
+            while (line != null)
+            {
+                string[] fields = line.Split('\t');
+                dataOutput.Rows.Add(fields);
+                line = r.ReadLine();
+            }
+        }
+    }
+}
