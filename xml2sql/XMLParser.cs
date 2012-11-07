42e9dc401c1f9741220ce5f6ff63e2e9f1493ea9
baibochun@gmail.com <baibochun@gmail.com@213476b3-89fe-de8f-e45c-0ea09cb4a6d9>
2012-11-07 03:11:41 +0000

    Init

---
+++ xml2sql/XMLParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.XPath;
+
+namespace xml2sql
+{
+    class XMLParser
+    {
+        private string filePath;
+
+        public XMLParser(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public List<List<string> > output(string loopFor, Dictionary<string, string> columns)
+        {
+            XPathDocument doc = new XPathDocument(this.filePath);
+            XPathNavigator nav = doc.CreateNavigator();
+            XPathNodeIterator iter = nav.Select(loopFor);
+            List<List<string>> rows = new List<List<string>>();
+            while (iter.MoveNext())
+            {
+                List<string> row = new List<string>();
+                foreach (KeyValuePair<string, string> pair in columns)
+                {
+                    XPathNodeIterator valueIter = iter.Current.Select(pair.Value);
+                    if (valueIter.MoveNext())
+                    {
+                        string value = valueIter.Current.Value;
+                        // Console.WriteLine("{0} = {1}", pair.Key, value);
+                        row.Add(value);
+                    }
+                    else
+                    {
+                        row.Add("");
+                    }
+                }
+                rows.Add(row);
+            }
+            return rows;
+        }
+    }
+}
